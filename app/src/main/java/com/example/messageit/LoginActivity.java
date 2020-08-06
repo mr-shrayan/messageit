c993e6808459384a9562fae39158d35a1854fd0d
Shrayan Bandyopadhyay <theemptyunexplored@gmail.com>
2020-08-07 01:28:09 +0530

    lining Login Screen & Register Screen p-8

--- app/src/main/java/com/example/messageit/LoginActivity.java
+++ app/src/main/java/com/example/messageit/LoginActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
 
@@ -13,10 +17,40 @@ public class LoginActivity extends AppCompatActivity
 
     private FirebaseUser currentUser;
 
+    private Button LoginButton, PhoneLoginButton;
+    private EditText UserEmail, UserPassword;
+    private TextView NeedNewAccountLink, ForgetPasswordLink;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        InitializeFields();
+
+
+        NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                SendUserToRegisterActivity();
+
+            }
+        });
+
+
+
+    }
+
+    private void InitializeFields()
+    {
+        LoginButton = (Button) findViewById(R.id.login_button);
+        PhoneLoginButton = (Button) findViewById(R.id.phone_login_button);
+        UserEmail = (EditText) findViewById(R.id.login_email);
+        UserPassword = (EditText) findViewById(R.id.login_password);
+        NeedNewAccountLink = (TextView) findViewById(R.id.need_new_account_link);
+        ForgetPasswordLink = (TextView) findViewById(R.id.forget_password_link);
+
     }
 
 
@@ -36,6 +70,12 @@ public class LoginActivity extends AppCompatActivity
     {
         Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class );
         startActivity(loginIntent);
+    }
+
+    private void SendUserToRegisterActivity()
+    {
+        Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class );
+        startActivity(registerIntent);
     }
 
 
--- app/src/main/java/com/example/messageit/RegisterActivity.java
+++ app/src/main/java/com/example/messageit/RegisterActivity.java
@@ -2,13 +2,58 @@ package com.example.messageit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity
+{
+
+
+    private Button CreateAccountButton;
+    private EditText UserEmail, UserPassword;
+    private TextView AlreadyHaveAccountLink;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        InitializeFields();
+
+        AlreadyHaveAccountLink.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+
+                SendUserToLoginActivity();
+
+            }
+        });
+
     }
+
+    private void InitializeFields()
+    {
+        CreateAccountButton = (Button) findViewById(R.id.register_button);
+        UserEmail = (EditText) findViewById(R.id.register_email);
+        UserPassword = (EditText) findViewById(R.id.register_password);
+        AlreadyHaveAccountLink = (TextView) findViewById(R.id.already_have_account_link);
+
+    }
+
+
+
+    private void SendUserToLoginActivity()
+    {
+        Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class );
+        startActivity(loginIntent);
+    }
+
 }
