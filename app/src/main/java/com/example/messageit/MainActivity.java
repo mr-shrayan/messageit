c3837393618c65e92a879170ee3886680812e95e
Shrayan Bandyopadhyay <theemptyunexplored@gmail.com>
2020-08-06 23:36:54 +0530

    Login Screen.

--- app/src/main/java/com/example/messageit/MainActivity.java
+++ app/src/main/java/com/example/messageit/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.messageit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -14,6 +16,8 @@ public class MainActivity extends AppCompatActivity
     private ViewPager myViewPager;
     private TabLayout myTabLayout;
     private TabsAccessorAdapter myTabsAccessorAdapter;
+
+    private FirebaseUser curretUser;
 
 
     @Override
@@ -39,4 +43,20 @@ public class MainActivity extends AppCompatActivity
     }
 
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (curretUser==null)
+        {
+            SendUserToLoginActivity();
+        }
+
+    }
+
+    private void SendUserToLoginActivity()
+    {
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class );
+        startActivity(loginIntent);
+    }
 }
