645ae999cdfdc2deff407a1e30cbc33fb3869c62
Shrayan Bandyopadhyay <theemptyunexplored@gmail.com>
2020-08-08 14:10:35 +0530

    Retrieving user info in settings page

--- app/src/main/java/com/example/messageit/SettingsActivity.java
+++ app/src/main/java/com/example/messageit/SettingsActivity.java
@@ -14,8 +14,11 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 
@@ -57,7 +60,10 @@ public class SettingsActivity extends AppCompatActivity
         });
 
 
+        RetrieveUserInfo();
+
     }
+
 
 
 
@@ -115,6 +121,44 @@ public class SettingsActivity extends AppCompatActivity
 
 
 
+    private void RetrieveUserInfo()
+    {
+        RootRef.child("Users").child(currentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot)
+            {
+                if ((snapshot.exists()) && (snapshot.hasChild("name") && (snapshot.hasChild("image"))))
+                {
+                    String retrieveUserName = snapshot.child("name").getValue().toString();
+                    String retrieveStatus = snapshot.child("status").getValue().toString();
+                    String retrieveProfileImage = snapshot.child("image").getValue().toString();
+
+                    userName.setText(retrieveUserName);
+                    userStatus.setText(retrieveStatus);
+                }
+                else if ((snapshot.exists()) && (snapshot.hasChild("name")))
+                {
+                    String retrieveUserName = snapshot.child("name").getValue().toString();
+                    String retrieveStatus = snapshot.child("status").getValue().toString();
+
+                    userName.setText(retrieveUserName);
+                    userStatus.setText(retrieveStatus);
+                }
+                else
+                {
+                    Toast.makeText(SettingsActivity.this, "Please set & update your Profile Information", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
+
     private void SendUserToMainActivity()
     {
         Intent mainIntent = new Intent(SettingsActivity.this, MainActivity.class );
@@ -122,7 +166,6 @@ public class SettingsActivity extends AppCompatActivity
         startActivity(mainIntent);
         finish();
     }
-
 
 
 
