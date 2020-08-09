d6636737290d0c711e68650efbda266cb3db0abb
Shrayan Bandyopadhyay <theemptyunexplored@gmail.com>
2020-08-10 03:21:31 +0530

    Group Chat Activity

---
+++ app/src/main/java/com/example/messageit/GroupChatActivity.java
@@ -0,0 +1,40 @@
+package com.example.messageit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
+
+public class GroupChatActivity extends AppCompatActivity
+{
+    private Toolbar mToolbar;
+    private ImageButton SendMessageButton;
+    private EditText userMessageInput;
+    private ScrollView mScrollView;
+    private TextView displayTextMessages;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_chat);
+
+        InitializeFields();
+    }
+
+    private void InitializeFields()
+    {
+        mToolbar = (Toolbar) findViewById(R.id.group_chat_bar_layout);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Group Name");
+
+        SendMessageButton = (ImageButton) findViewById(R.id.send_message_button);
+        userMessageInput = (EditText) findViewById(R.id.input_group_message);
+        displayTextMessages = (TextView) findViewById(R.id.group_chat_text_display);
+        mScrollView = (ScrollView) findViewById(R.id.my_scroll_view);
+    }
+}
