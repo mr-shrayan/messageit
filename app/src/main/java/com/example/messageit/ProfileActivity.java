4bd889b1f320e5e579cfbb09519b8684b7689a19
Shrayan Bandyopadhyay <theemptyunexplored@gmail.com>
2020-08-18 22:03:13 +0530

    Find Friends Activity INSIDE work 5 pt-33

--- app/src/main/java/com/example/messageit/ProfileActivity.java
+++ app/src/main/java/com/example/messageit/ProfileActivity.java
@@ -1,13 +1,29 @@
 package com.example.messageit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class ProfileActivity extends AppCompatActivity
 {
     private String receiverUserId;
+
+    private TextView userProfileName, userProfileStatus;
+    private Button sendMessageRequestButton;
+
+    private DatabaseReference UserRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -16,9 +32,41 @@ public class ProfileActivity extends AppCompatActivity
         setContentView(R.layout.activity_profile);
 
 
+        UserRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
         receiverUserId = getIntent().getExtras().get("visit_user_id").toString();
 
-        Toast.makeText(this, "UserID: " + receiverUserId, Toast.LENGTH_SHORT).show();
+        userProfileName = (TextView) findViewById(R.id.visit_user_name);
+        userProfileStatus = (TextView) findViewById(R.id.visit_profile_status);
+        sendMessageRequestButton = (Button) findViewById(R.id.send_message_request_button);
+
+
+        RetrieveUserInfo();
+    }
+
+    private void RetrieveUserInfo()
+    {
+        UserRef.child(receiverUserId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot)
+            {
+                if ((snapshot.exists())  && (snapshot.hasChild("name")))
+                {
+                    String userName = snapshot.child("name").getValue().toString();
+                    String userStatus = snapshot.child("status").getValue().toString();
+
+                    userProfileName.setText(userName);
+                    userProfileStatus.setText(userStatus);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error)
+            {
+
+            }
+        });
     }
 
 }
