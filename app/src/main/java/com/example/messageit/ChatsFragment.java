e45a2f4d5f3d39bfcd7472b734a53dbeb67ec8d0
Shrayan Bandyopadhyay <theemptyunexplored@gmail.com>
2020-08-24 23:57:56 +0530

    Chat Fragment work- pt-45

--- app/src/main/java/com/example/messageit/ChatsFragment.java
+++ app/src/main/java/com/example/messageit/ChatsFragment.java
@@ -2,63 +2,118 @@ package com.example.messageit;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ChatsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ChatsFragment extends Fragment {
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class ChatsFragment extends Fragment
+{
+    private View PrivateChatsView;
+    private RecyclerView chatsList;
+    private DatabaseReference ChatsRef, UsersRef;
+    private FirebaseAuth mAuth;
+    private String currentUserId;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ChatsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChatsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ChatsFragment newInstance(String param1, String param2) {
-        ChatsFragment fragment = new ChatsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public ChatsFragment()
+    {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats, container, false);
+                             Bundle savedInstanceState)
+    {
+        PrivateChatsView = inflater.inflate(R.layout.fragment_chats, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserId = mAuth.getCurrentUser().getUid();
+        ChatsRef = FirebaseDatabase.getInstance().getReference().child("Contacts").child(currentUserId);
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        chatsList = (RecyclerView) PrivateChatsView.findViewById(R.id.chat_list);
+        chatsList.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        return PrivateChatsView;
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Contacts> options =
+                new FirebaseRecyclerOptions.Builder<Contacts>()
+                .setQuery(ChatsRef, Contacts.class)
+                .build();
+
+
+        FirebaseRecyclerAdapter<Contacts, ChatsViewHolder > adapter =
+                new FirebaseRecyclerAdapter<Contacts, ChatsViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull final ChatsViewHolder holder, int position, @NonNull Contacts model)
+                    {
+                        final String usersIDs = getRef(position).getKey();
+
+                        UsersRef.child(usersIDs).addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot)
+                            {
+                                final String retName = snapshot.child("name").getValue().toString();
+                                final String retStatus = snapshot.child("status").getValue().toString();
+
+
+                                holder.userName.setText(retName);
+                                holder.userStatus.setText("Last Seen: " + "\n" + "Date: " + " Time");
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+                    }
+
+                    @NonNull
+                    @Override
+                    public ChatsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+                    {
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.users_display_layout, parent, false);
+                        return new ChatsViewHolder(view);
+                    }
+                };
+        chatsList.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+
+
+
+    public static class ChatsViewHolder extends RecyclerView.ViewHolder
+    {
+
+        TextView userStatus, userName;
+
+
+        public ChatsViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+
+            userName = itemView.findViewById(R.id.user_profile_name);
+            userStatus = itemView.findViewById(R.id.user_status);
+        }
     }
 }
