a877dc41fc85549048af0ac70c56f0e96ca77f96
Shrayan Bandyopadhyay <theemptyunexplored@gmail.com>
2020-08-31 19:56:14 +0530

    Revert "Notification work 2 (Getting Device Token) pt-52"
    This reverts commit 8a248540

--- app/src/main/java/com/example/messageit/RegisterActivity.java
+++ app/src/main/java/com/example/messageit/RegisterActivity.java
@@ -20,7 +20,6 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.iid.FirebaseInstanceId;
 
 public class RegisterActivity extends AppCompatActivity
 {
@@ -99,14 +98,8 @@ public class RegisterActivity extends AppCompatActivity
                 {
                     if (task.isSuccessful())
                     {
-                        String deviceToken = FirebaseInstanceId.getInstance().getToken();
-
-
                         String currentUserID = mAuth.getCurrentUser().getUid();
                         RootRef.child("Users").child(currentUserID).setValue("");
-
-                        RootRef.child("Users").child(currentUserID).child("device_token")
-                                .setValue(deviceToken);
 
                         SendUserToMainActivity();
                         Toast.makeText(RegisterActivity.this, "Account Successfully Created!", Toast.LENGTH_SHORT).show();
