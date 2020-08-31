d4fd72759966d30a9b6e9abe58e7a1221d4cbe88
Annonymous-sys <sbanerjee22@outlook.com>
2020-08-31 14:57:27 +0530

    Test

--- app/src/main/java/com/example/messageit/ChatActivity.java
+++ app/src/main/java/com/example/messageit/ChatActivity.java
@@ -61,6 +61,7 @@ public class ChatActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
